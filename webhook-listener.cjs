41fce53c157aeb567f62aef97c8fffa5a14345b0
TeekingHamzat <teekinghamzat@gmail.com>
2026-01-03 07:57:06 +0100

    chore: Update webhook listener port from 3000 to 4000.

--- webhook-listener.cjs
+++ webhook-listener.cjs
@@ -30,4 +30,4 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => console.log("Webhook listener running on port 3000"));
+server.listen(4000, () => console.log("Webhook listener running on port 4000"));
